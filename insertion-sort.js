28a47f215c6a990d4f23c39302a3edabfaf75927
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-19 22:03:19 -0300

    refactor: função nova adicionada 'toExchange()'

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,14 +1,11 @@
 const livros = require('./list-livros')
+const toExchange = require('./exchange')
 
 function insertionSort (lista) {
   for (let i = 0; i < lista.length; i++) {
     let analise = i
     while (analise > 0 && lista[analise].preco < lista[analise - 1].preco) {
-      let itemAnalise = lista[analise]
-      let itemAnterior = lista[analise - 1]
-
-      lista[analise] = itemAnterior
-      lista[analise - 1] = itemAnalise
+      toExchange(lista, analise)
 
       analise--
     }
